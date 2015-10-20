9c5e258812825ce382eb2dcb5a60e9d009fa696a
lausai <lausai360@gmail.com>
2015-10-20 17:03:51 +0800

    test push

--- VmHelper/Program.cs
+++ VmHelper/Program.cs
@@ -16,6 +16,7 @@ namespace VmHelper
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
         }
     }
 }
